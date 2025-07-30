684bccfd89b020368ec10babb02aa0faf957c64e
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-07-30 19:59:44 +0530

    Add solution for 4Sum

---
+++ solutions/medium/4Sum.java
@@ -0,0 +1,6 @@
+// Title: 4Sum
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/4sum/
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
