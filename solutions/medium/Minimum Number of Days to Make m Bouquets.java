39ffd340fb38a325d144f22a2e13083536108472
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-02 19:34:26 +0530

    Add solution for Minimum Number of Days to Make m Bouquets

---
+++ solutions/medium/Minimum Number of Days to Make m Bouquets.java
@@ -0,0 +1,30 @@
+// Title: Minimum Number of Days to Make m Bouquets
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+
+                    if (curr==k){
+                        cnt++;
+                    } 
+                        curr=0;
+                }
+                    curr++;
+            int curr=0;
+            for (int i=0 ; i<bloomDay.length ; i++){
+                if (bloomDay[i]<=mid){
+            int mid = (left+right)/2;
+            int cnt=0;
+        while(left<=right){
+
+        int ans=right;
+
+            right=Math.max(right,bloomDay[i]);
+        }
+        int right = 1;
+        for (int i=0 ; i<bloomDay.length ; i++){
+        int left = 1;
+        }
+            return -1;
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long)m*(long)k>bloomDay.length){
+class Solution {
