2e73ae1b88435e31fe64a70723911551f5710347
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-04 02:47:11 +0530

    Add solution for Maximum Fruits Harvested After at Most K Steps

---
+++ solutions/hard/Maximum Fruits Harvested After at Most K Steps.java
@@ -0,0 +1,28 @@
+// Title: Maximum Fruits Harvested After at Most K Steps
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-fruits-harvested-after-at-most-k-steps/
+
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public int upperBound(int[] arr, int left, int right, int val) {
+        int res = right + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > val) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
