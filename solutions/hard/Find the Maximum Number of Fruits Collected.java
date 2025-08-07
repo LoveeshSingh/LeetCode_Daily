400b956fc3468839e431512e0e17c663bcbc70f2
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-08 03:14:48 +0530

    Add solution for Find the Maximum Number of Fruits Collected

---
+++ solutions/hard/Find the Maximum Number of Fruits Collected.java
@@ -0,0 +1,30 @@
+// Title: Find the Maximum Number of Fruits Collected
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-maximum-number-of-fruits-collected/
+
+        // System.out.println();
+        // }
+        //     System.out.println();
+        //     }
+        //         System.out.print(child3[i][j]+" ");
+        //     for (int j=0 ; j<n ; j++){
+        // for (int i=0 ; i<n ; i++){
+        // System.out.println();
+        //     }
+        //     System.out.println();
+        // }
+        //         System.out.print(child2[i][j]+" ");
+        //     for (int j=0 ; j<n ; j++){
+        // for (int i=0 ; i<n ; i++){
+        }
+            }
+                else child3[i][j]=fruits[i][j];
+                if (i==j) child3[i][j]=0;
+            for (int i=j ; i>=n-j-1 ; i--){
+        int ans=0;
+        for (int i=0 ; i<n ; i++){
+            ans+=fruits[i][i];
+        }
+        int[][] dir2= new int[][]{{-1,-1},{0,-1},{1,-1}};
+        int[][] dir3= new int[][]{{-1,-1},{-1,0},{-1,1}};
