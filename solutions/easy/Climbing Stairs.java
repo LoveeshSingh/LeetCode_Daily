46d28f56a4a0ebf56f348ff5eed2eb226a72d322
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-07 22:32:58 +0530

    Add solution for Climbing Stairs

---
+++ solutions/easy/Climbing Stairs.java
@@ -0,0 +1,16 @@
+// Title: Climbing Stairs
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        for (int i=2 ; i<=n ; i++){
+        dp[1]=1;
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+    }
+        return dp[n];
+}
