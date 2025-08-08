6ede61fca11d1da66ac2b750bd67b4100893cd6f
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-08 14:06:21 +0530

    Add solution for Soup Servings

---
+++ solutions/medium/Soup Servings.java
@@ -0,0 +1,29 @@
+// Title: Soup Servings
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/soup-servings/
+
+            return 0.5;
+        }
+        else if(a<=0){
+            return 1;
+        }
+        else if (b<=0){
+            return 0;
+        }
+        if (map.containsKey(Arrays.asList(a,b))){
+            return map.get(Arrays.asList(a,b));
+        }
+        for (int[] dec : decr){
+        double ans=0;
+        if(a<=0 && b<=0){
+    }
+
+    public double probab(int a , int b , int[][] decr){
+        int[][] decr = new int[][]{{-100,0},{-75,-25},{-50,-50},{-25,-75}};
+        map = new HashMap<>();
+        return probab(n,n,decr);
+        if (n>4800) return 1.0;
+    HashMap<List<Integer>,Double> map;
+    public double soupServings(int n) {
+class Solution {
