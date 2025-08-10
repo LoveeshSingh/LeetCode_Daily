aa1024bdc25a1016d0371220ebc4bbf655ee3570
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-10 20:33:58 +0530

    Add solution for Reordered Power of 2

---
+++ solutions/medium/Reordered Power of 2.java
@@ -0,0 +1,20 @@
+// Title: Reordered Power of 2
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/reordered-power-of-2/
+
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        List<String> list = Arrays.asList(String.valueOf(n).split(""));
+        Collections.sort(list);
+        for (int i=0 ; i<=29 ; i++){
+            num<<=1;
+        }
+    }
+        int num = 1;
+            if (list.equals(powerOf2)) return true;
+        return false;
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌//·‌System.out.println(num);
+            List<String> powerOf2 = Arrays.asList(String.valueOf(num).split(""));
+            Collections.sort(powerOf2);
+}
