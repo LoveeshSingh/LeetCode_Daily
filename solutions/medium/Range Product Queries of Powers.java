005d97bfc4e68a3b27f82aa5452d20372fd27870
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-12 03:39:12 +0530

    Add solution for Range Product Queries of Powers

---
+++ solutions/medium/Range Product Queries of Powers.java
@@ -0,0 +1,29 @@
+// Title: Range Product Queries of Powers
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/range-product-queries-of-powers/
+
+//     public int modExp(int a , int b , int c){ // a^b%c
+
+//     }
+//         return ans;
+//             num++;
+//         }
+//         for (int value : power) System.out.println(value+" ");
+//         int[] ans = new int[queries.length];
+//         for (int i=0 ; i<queries.length ; i++){
+//             int cnt=power[queries[i][1]+1]-power[queries[i][0]];
+//             ans[i]=modExp(2,cnt,mod);
+//         }
+//         int num=0;
+//         for (int i=0 ; i<bin.length() ; i++){
+//             if (bin.charAt(i)=='1') power[index]=power[index++-1]+num;
+//         int[] power = new int[Integer.bitCount(n)+1];
+//         power[0]=0;
+//         int index=1;
+//         String reversed = Integer.toBinaryString(n);
+//         String bin = new StringBuilder(reversed).reverse().toString();
+//         System.out.println(bin);
+//     final int mod = 1000000007;
+//     public int[] productQueries(int n, int[][] queries) {
+// class Solution {
