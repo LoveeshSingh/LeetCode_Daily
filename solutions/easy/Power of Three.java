bf993668f2b50f7b1c70c5ff6947ed7ba24f2665
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-13 13:52:53 +0530

    Add solution for Power of Three

---
+++ solutions/easy/Power of Three.java
@@ -0,0 +1,10 @@
+// Title: Power of Three
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/power-of-three/
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+    }
+        return (n>0)&&(1162261467%n==0);
+}
