a6cd3f6072aea71d846818c2bff9caf0bc6c8ad5
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-17 23:41:29 +0530

    Add solution for New 21 Game

---
+++ solutions/medium/New 21 Game.java
@@ -0,0 +1,26 @@
+// Title: New 21 Game
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/new-21-game/
+
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        double dp[] = new double[n + 1];
+        dp[0] = 1;
+        double s = k > 0 ? 1 : 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = s / maxPts;
+            if (i < k) {
+                s += dp[i];
+            }
+            if (i - maxPts >= 0 && i - maxPts < k) {
+                s -= dp[i - maxPts];
+            }
+        }
+        double ans = 0;
+        for (int i = k; i <= n; i++) {
+            ans += dp[i];
+        }
+        return ans;
+    }
+}
