abf1481ec5c4eb7042f1844b6655868f3ebf312a
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-26 18:50:34 +0530

    Add solution for Maximize the Number of Target Nodes After Connecting Trees I

---
+++ solutions/medium/Maximize the Number of Target Nodes After Connecting Trees I.java
@@ -0,0 +1,6 @@
+// Title: Maximize the Number of Target Nodes After Connecting Trees I
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/
+
+        int[] count2 = build(edges2, k - 1);
