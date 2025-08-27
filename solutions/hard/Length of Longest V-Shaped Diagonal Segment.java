76d399c8656898d6632e3e72b6a3e266c9ad51a7
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-28 00:49:13 +0530

    Add solution for Length of Longest V-Shaped Diagonal Segment

---
+++ solutions/hard/Length of Longest V-Shaped Diagonal Segment.java
@@ -0,0 +1,29 @@
+// Title: Length of Longest V-Shaped Diagonal Segment
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/length-of-longest-v-shaped-diagonal-segment/
+
+            }
+                }
+                    Arrays.fill(memo[i][j][k], -1);
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 4; k++) {
+        this.n = grid[0].length;
+        this.memo = new int[m][n][4][2];
+
+        for (int i = 0; i < m; i++) {
+    public int lenOfVDiagonal(int[][] grid) {
+        this.grid = grid;
+        this.m = grid.length;
+    private int m, n;
+
+    private int[][] grid;
+    private int[][][][] memo;
+    };
+        { -1, 1 },
+        { -1, -1 },
+        { 1, -1 },
+    private static final int[][] DIRS = {
+        { 1, 1 },
+class Solution {
+
