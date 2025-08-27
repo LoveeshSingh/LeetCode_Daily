a3e6f0f79464d0c56797e887bb0a60bc44e82957
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-27 11:30:45 +0530

    Add solution for Asteroid Collision

---
+++ solutions/medium/Asteroid Collision.java
@@ -0,0 +1,30 @@
+// Title: Asteroid Collision
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/asteroid-collision/
+
+                else if(top+asteroids[i]==0){
+                    break;
+                }
+                else{
+                    if (stk.isEmpty()){
+                }
+                    break;
+                    stk.push(top);
+                else if(top+asteroids[i]>0){
+                }
+                    break;
+                    stk.push(asteroids[i]);
+                    stk.push(top);
+                if (top<0 || asteroids[i]>0){
+                int top = stk.pop();
+            while(!stk.isEmpty()){
+            }
+                        stk.push(asteroids[i]);
+                continue;
+                stk.push(asteroids[i]);
+            if (stk.isEmpty()){
+                        break;
+                    }
+                }
+            }
