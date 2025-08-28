51f50d96aade0d278d961b130f72ee46fbd64d91
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-28 10:27:59 +0530

    Add solution for Sort Matrix by Diagonals

---
+++ solutions/medium/Sort Matrix by Diagonals.java
@@ -0,0 +1,6 @@
+// Title: Sort Matrix by Diagonals
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/sort-matrix-by-diagonals/
+
+class Solution {
