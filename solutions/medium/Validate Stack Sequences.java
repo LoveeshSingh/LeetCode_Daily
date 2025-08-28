fd1ef673129e3e57b4864fc286bba360ce4a02e7
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-28 11:18:49 +0530

    Add solution for Validate Stack Sequences

---
+++ solutions/medium/Validate Stack Sequences.java
@@ -0,0 +1,23 @@
+// Title: Validate Stack Sequences
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/validate-stack-sequences/
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stk = new Stack<>();
+        while(i<popped.length){
+
+        }
+        int j=0;
+        int i=0;
+        return true;
+            while(stk.isEmpty() || stk.peek()!=popped[i]){
+                if (j>=pushed.length) return false;
+            }
+    }
+                stk.push(pushed[j++]);
+            stk.pop();
+            i++;
+            // System.out.println(stk.peek());
+}
