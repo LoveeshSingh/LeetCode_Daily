84a52199a6e2709275fc98770b5d291fcfee6474
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-31 00:18:49 +0530

    Add solution for Find the K-th Character in String Game II

---
+++ solutions/hard/Find the K-th Character in String Game II.java
@@ -0,0 +1,6 @@
+// Title: Find the K-th Character in String Game II
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-k-th-character-in-string-game-ii/
+
+class Solution {
