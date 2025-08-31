6087060d248c40d098c60c365c7ff121c247e0a4
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-08-31 17:12:37 +0530

    Add solution for Lowest Common Ancestor of a Binary Search Tree

---
+++ solutions/medium/Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,7 @@
+// Title: Lowest Common Ancestor of a Binary Search Tree
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+ *     TreeNode left;
+ *     TreeNode right;
