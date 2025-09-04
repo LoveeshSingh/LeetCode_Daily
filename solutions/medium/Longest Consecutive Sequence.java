372a85f27db6798b8933e862a75019bdfd5d3e28
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-04 19:17:06 +0530

    Add solution for Longest Consecutive Sequence

---
+++ solutions/medium/Longest Consecutive Sequence.java
@@ -0,0 +1,6 @@
+// Title: Longest Consecutive Sequence
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution {
