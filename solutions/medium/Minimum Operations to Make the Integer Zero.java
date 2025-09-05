4306c9c53c60165dc119a7e42783a52151c58bdf
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-06 02:22:58 +0530

    Add solution for Minimum Operations to Make the Integer Zero

---
+++ solutions/medium/Minimum Operations to Make the Integer Zero.java
@@ -0,0 +1,18 @@
+// Title: Minimum Operations to Make the Integer Zero
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-operations-to-make-the-integer-zero/
+
+class Solution {
+    public int makeTheIntegerZero(int num1, int num2) {
+        int cnt=1;
+        while(n1>=cnt && cnt<=35){
+            if (Long.bitCount(n1)<=cnt) return cnt;
+        }
+    }
+        n1-=num2;
+        return -1;
+            n1-=num2;
+            cnt++;
+        long n1=num1;
+}
