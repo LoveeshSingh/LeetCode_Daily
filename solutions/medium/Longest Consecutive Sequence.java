2c252badc1670ecd21929ab1a577e6d4f186c5f9
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-05 16:59:59 +0530

    Add solution for Longest Consecutive Sequence

--- solutions/medium/Longest Consecutive Sequence.java
+++ solutions/medium/Longest Consecutive Sequence.java
@@ -3,4 +3,26 @@
             // Language: Java
             // Link: https://leetcode.com/problems/longest-consecutive-sequence/
 
+            parent[parent2]=parent1;
+            size[parent1]+=size[parent2];
+        }  
+    }
+}
+
 class Solution {
+    public int longestConsecutive(int[] nums) {
+        DSU dsu = new DSU(nums.length);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans=0;
+        for (int i=0 ; i<nums.length ; i++){
+            if (map.containsKey(nums[i])) continue;
+            map.put(nums[i],val++);
+            if (map.containsKey(nums[i]-1)) dsu.union(map.get(nums[i]),map.get(nums[i]-1));
+            if (map.containsKey(nums[i]+1)) dsu.union(map.get(nums[i]),map.get(nums[i]+1));
+            ans=Math.max(ans,dsu.size[dsu.find(map.get(nums[i]))]);
+        }
+    }
+        else{
+        return ans;
+        int val=0;
+}
