3f97389f769dd62eda2ad052f57b64e794a343b2
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-06 10:56:25 +0530

    Add solution for Minimum Operations to Make Array Elements Zero

---
+++ solutions/hard/Minimum Operations to Make Array Elements Zero.java
@@ -0,0 +1,30 @@
+// Title: Minimum Operations to Make Array Elements Zero
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-operations-to-make-array-elements-zero/
+
+    public long minOperations(int[][] queries) {
+        for (int[] query : queries){
+            int right = query[1];
+        }  
+            int left = query[0];
+            for (int i=1 ; i<=15 ; i++){
+            }
+            int pwr = 4;
+                if (left<pwr){
+                    if (right<pwr) val=right-left+1;
+                }
+    }
+                pwr<<=2;
+            long cnt=0;
+        long ans=0;
+                int val=0;
+                    else val=pwr-left;
+                    left=pwr;
+                    cnt+=((long)val)*i;
+                    if (right<pwr) break;
+class Solution {
+            ans+=(cnt+1)/2;
+        return ans;          
+            // System.out.println(cnt);
+}
