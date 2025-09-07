341838cc68fa8164a12b75755941decb7586dbda
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-07 08:03:22 +0530

    Add solution for Minimum Operations to Equalize Array

---
+++ solutions/easy/Minimum Operations to Equalize Array.java
@@ -0,0 +1,5 @@
+// Title: Minimum Operations to Equalize Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-operations-to-equalize-array/
+
