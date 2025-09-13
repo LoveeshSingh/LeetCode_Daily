849b1a53e1d916d4cfe9bfd37514a5d70bb83028
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-13 20:04:29 +0530

    Add solution for Smallest Absent Positive Greater Than Average

---
+++ solutions/easy/Smallest Absent Positive Greater Than Average.java
@@ -0,0 +1,5 @@
+// Title: Smallest Absent Positive Greater Than Average
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/smallest-absent-positive-greater-than-average/
+
