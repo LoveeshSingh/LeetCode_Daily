3fd43b0b711e70f8c71590ff8b9f3260e0eccdfa
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-14 08:04:34 +0530

    Add solution for Earliest Time to Finish One Task

---
+++ solutions/easy/Earliest Time to Finish One Task.java
@@ -0,0 +1,5 @@
+// Title: Earliest Time to Finish One Task
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/earliest-time-to-finish-one-task/
+
