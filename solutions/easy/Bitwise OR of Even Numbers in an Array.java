09434bb28fd1bd2220836713bbf0704a974ec6a4
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-21 08:01:48 +0530

    Add solution for Bitwise OR of Even Numbers in an Array

---
+++ solutions/easy/Bitwise OR of Even Numbers in an Array.java
@@ -0,0 +1,5 @@
+// Title: Bitwise OR of Even Numbers in an Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/bitwise-or-of-even-numbers-in-an-array/
+
