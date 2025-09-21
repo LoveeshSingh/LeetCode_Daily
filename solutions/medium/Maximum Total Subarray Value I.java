9399c0d134077bc7cb95ed10a95bb3c5de2b982b
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-21 08:30:49 +0530

    Add solution for Maximum Total Subarray Value I

---
+++ solutions/medium/Maximum Total Subarray Value I.java
@@ -0,0 +1,5 @@
+// Title: Maximum Total Subarray Value I
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-total-subarray-value-i/
+
