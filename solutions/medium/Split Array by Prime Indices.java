fb31c4e06e8052df11efdea19942ec96392b63bb
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-23 11:02:37 +0530

    Add solution for Split Array by Prime Indices

---
+++ solutions/medium/Split Array by Prime Indices.java
@@ -0,0 +1,5 @@
+// Title: Split Array by Prime Indices
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/split-array-by-prime-indices/
+
