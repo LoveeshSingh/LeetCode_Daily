4a1b25fbab7b8cb02738d4aae683d5e66dbec9b9
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-28 00:31:57 +0530

    Add solution for Maximize Alternating Sum Using Swaps

---
+++ solutions/hard/Maximize Alternating Sum Using Swaps.java
@@ -0,0 +1,7 @@
+// Title: Maximize Alternating Sum Using Swaps
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximize-alternating-sum-using-swaps/
+
+ 
+ 
