35b5a06866b91b5fdcd11b98cdd15d7cb745501f
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-27 20:12:13 +0530

    Add solution for Majority Frequency Characters

---
+++ solutions/easy/Majority Frequency Characters.java
@@ -0,0 +1,5 @@
+// Title: Majority Frequency Characters
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/majority-frequency-characters/
+
