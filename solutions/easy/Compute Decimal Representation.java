ef518e7119d5344965428403a34cbc757dbe1352
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-09-28 08:07:03 +0530

    Add solution for Compute Decimal Representation

---
+++ solutions/easy/Compute Decimal Representation.java
@@ -0,0 +1,5 @@
+// Title: Compute Decimal Representation
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/compute-decimal-representation/
+
