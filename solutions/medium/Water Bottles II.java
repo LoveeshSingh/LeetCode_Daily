0ecc4c6b8f1ec3d25ab57c9f22aa633b0c484fc3
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-02 13:23:02 +0530

    Add solution for Water Bottles II

--- solutions/medium/Water Bottles II.java
+++ solutions/medium/Water Bottles II.java
@@ -3,22 +3,4 @@
             // Language: Java
             // Link: https://leetcode.com/problems/water-bottles-ii/
 
-class Solution {
-    public int maxBottlesDrunk(int numBottles, int numExchange) {
-        int full = numBottles;
-        int empty = 0;
-        int exchng = numExchange;
-        while(full>0){
-            while(empty>=exchng){
-        }
-                full+=1;
-            }
-    }
-            ans+=full;
-        int ans=0;
-            empty+=full;
-            full=0;
-                empty-=exchng;
-                exchng++;
-        return ans;
-}
+c
