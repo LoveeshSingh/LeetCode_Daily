2ab8b8ede3aeaa6de5f989f0a389f4fdd939baae
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-12 08:02:50 +0530

    Add solution for Sum of Elements With Frequency Divisible by K

---
+++ solutions/easy/Sum of Elements With Frequency Divisible by K.java
@@ -0,0 +1,5 @@
+// Title: Sum of Elements With Frequency Divisible by K
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/sum-of-elements-with-frequency-divisible-by-k/
+
