0df8703732601bc44da7a884cb4163bf979b132f
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-12 08:26:51 +0530

    Add solution for Longest Balanced Substring I

---
+++ solutions/medium/Longest Balanced Substring I.java
@@ -0,0 +1,5 @@
+// Title: Longest Balanced Substring I
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/longest-balanced-substring-i/
+
