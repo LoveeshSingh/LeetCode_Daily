8e362f2c68ee9e9a063bce4f0cab8a3eddd5c5c0
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-19 08:37:29 +0530

    Add solution for Lexicographically Smallest Permutation Greater Than Target

---
+++ solutions/medium/Lexicographically Smallest Permutation Greater Than Target.java
@@ -0,0 +1,5 @@
+// Title: Lexicographically Smallest Permutation Greater Than Target
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/lexicographically-smallest-permutation-greater-than-target/
+
