ee451326739371ce4deff4a0ad14a8e635768866
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-23 22:44:37 +0530

    Add solution for Check If Digits Are Equal in String After Operations I

---
+++ solutions/easy/Check If Digits Are Equal in String After Operations I.java
@@ -0,0 +1,6 @@
+// Title: Check If Digits Are Equal in String After Operations I
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/
+
+c
