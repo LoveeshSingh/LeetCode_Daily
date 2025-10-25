3faa747a4308a21ceb13c43e1dea12dad3285c83
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-25 20:14:58 +0530

    Add solution for Lexicographically Smallest String After Reverse

---
+++ solutions/medium/Lexicographically Smallest String After Reverse.java
@@ -0,0 +1,5 @@
+// Title: Lexicographically Smallest String After Reverse
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/lexicographically-smallest-string-after-reverse/
+
