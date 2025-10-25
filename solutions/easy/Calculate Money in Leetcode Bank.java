2162d3f1f07f7b2358ed3124de3c5c1399595739
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-26 00:52:09 +0530

    Add solution for Calculate Money in Leetcode Bank

---
+++ solutions/easy/Calculate Money in Leetcode Bank.java
@@ -0,0 +1,21 @@
+// Title: Calculate Money in Leetcode Bank
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+
+class Solution {
+    public int totalMoney(int n) {
+        int k = n / 7;
+        int F = 28;
+        int L = 28 + (k - 1) * 7;
+        int arithmeticSum = k * (F + L) / 2;
+        
+        int monday = 1 + k;
+        int finalWeek = 0;
+        for (int day = 0; day < n % 7; day++) {
+            finalWeek += monday + day;
+        }
+        
+        return arithmeticSum + finalWeek;
+    }
+}
