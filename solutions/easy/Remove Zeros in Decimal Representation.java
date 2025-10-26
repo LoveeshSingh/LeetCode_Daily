152629e8bebc546db86f25aa500d402810c03d2f
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-26 08:04:32 +0530

    Add solution for Remove Zeros in Decimal Representation

---
+++ solutions/easy/Remove Zeros in Decimal Representation.java
@@ -0,0 +1,5 @@
+// Title: Remove Zeros in Decimal Representation
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/remove-zeros-in-decimal-representation/
+
