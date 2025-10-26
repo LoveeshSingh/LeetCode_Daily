378a0075b693ae46ce2019c9d88fde34ff61a0cc
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-10-26 09:11:46 +0530

    Add solution for Count Distinct Subarrays Divisible by K in Sorted Array

---
+++ solutions/hard/Count Distinct Subarrays Divisible by K in Sorted Array.java
@@ -0,0 +1,5 @@
+// Title: Count Distinct Subarrays Divisible by K in Sorted Array
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/count-distinct-subarrays-divisible-by-k-in-sorted-array/
+
