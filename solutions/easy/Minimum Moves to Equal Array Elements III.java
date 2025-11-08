c1d8cb846c589c0d5039b642bcb85b22cea71c7d
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-11-08 20:04:21 +0530

    Add solution for Minimum Moves to Equal Array Elements III

---
+++ solutions/easy/Minimum Moves to Equal Array Elements III.java
@@ -0,0 +1,5 @@
+// Title: Minimum Moves to Equal Array Elements III
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-iii/
+
