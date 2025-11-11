6bf9cfcabba172202d8724d71f13a6a5a6658a63
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-11-11 22:37:16 +0530

    Add solution for Maximum Count of Positive Integer and Negative Integer

---
+++ solutions/easy/Maximum Count of Positive Integer and Negative Integer.java
@@ -0,0 +1,5 @@
+// Title: Maximum Count of Positive Integer and Negative Integer
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/
+
