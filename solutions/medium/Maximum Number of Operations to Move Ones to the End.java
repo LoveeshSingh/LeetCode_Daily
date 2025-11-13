b0ed0e59e65eecff431e273c310bf1ce1e8bb6fb
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-11-13 13:37:54 +0530

    Add solution for Maximum Number of Operations to Move Ones to the End

---
+++ solutions/medium/Maximum Number of Operations to Move Ones to the End.java
@@ -0,0 +1,24 @@
+// Title: Maximum Number of Operations to Move Ones to the End
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/
+
+class Solution {
+    public int maxOperations(String s) {
+        int ans = 0;
+        int cnt=0;
+        for (int i=s.length()-1 ; i>=0 ; i--){
+        }
+        return ans;
+            if (s.charAt(i)=='1'){
+                ans+=cnt;
+            }
+            else{
+                while(i>=0 && s.charAt(i)=='0'){
+            }
+                    i--;
+                }
+    }
+                cnt++;
+                i++;
+}
