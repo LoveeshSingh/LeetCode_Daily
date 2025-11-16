63cc0c445b9f444239ce97fa3b682c5691cc99c5
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-11-16 08:02:46 +0530

    Add solution for Maximize Expression of Three Elements

---
+++ solutions/easy/Maximize Expression of Three Elements.java
@@ -0,0 +1,5 @@
+// Title: Maximize Expression of Three Elements
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximize-expression-of-three-elements/
+
