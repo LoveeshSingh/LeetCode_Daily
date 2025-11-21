5e2466d7782697481bc3b24a38297855831ef355
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-11-22 02:13:54 +0530

    Add solution for Unique Length-3 Palindromic Subsequences

--- solutions/medium/Unique Length-3 Palindromic Subsequences.java
+++ solutions/medium/Unique Length-3 Palindromic Subsequences.java
@@ -3,4 +3,25 @@
             // Language: Java
             // Link: https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
 
-c
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int[] first = new int[26];
+        Arrays.fill(first,-1);
+        int[] last = new int[26];
+        for (int i=0 ; i<s.length() ; i++){
+            if (first[val]==-1) first[val] = i;
+        }
+        Arrays.fill(last,-1);
+            int val = s.charAt(i)-'a';
+            last[val] = i;
+        for (int i=0 ; i<26 ; i++){
+            for(int j=first[i]+1 ; j<last[i] ; j++){
+        }
+                set.add(s.charAt(j));
+            }
+    }
+            HashSet<Character>set=new HashSet<>();
+        int cnt=0;
+            cnt+=set.size();
+        return cnt;
+}
