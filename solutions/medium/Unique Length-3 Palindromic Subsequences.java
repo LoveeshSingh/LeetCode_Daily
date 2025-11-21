fe87bbea5619facb3ae4b66b75e2962e647affad
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-11-21 12:25:48 +0530

    Add solution for Unique Length-3 Palindromic Subsequences

---
+++ solutions/medium/Unique Length-3 Palindromic Subsequences.java
@@ -0,0 +1,6 @@
+// Title: Unique Length-3 Palindromic Subsequences
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
+
+c
