5805bf4b2c0feb1c42277932a7f8caf152e64de8
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-03 22:18:22 +0530

    Add solution for Count Number of Trapezoids II

---
+++ solutions/hard/Count Number of Trapezoids II.java
@@ -0,0 +1,33 @@
+// Title: Count Number of Trapezoids II
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/count-number-of-trapezoids-ii/
+
+            }
+                    .add(k);
+                    .computeIfAbsent(mid, key -> new ArrayList<>())
+                }
+                if (b == -0.0) {
+                    b = 0.0;
+                }
+                int mid = (x1 + x2) * 10000 + (y1 + y2);
+                slopeToIntercept
+                    .computeIfAbsent(k, key -> new ArrayList<>())
+                    .add(b);
+                midToSlope
+                    k = 0.0;
+                if (k == -0.0) {
+                }
+                int dy = y1 - y2;
+                double k;
+                double b;
+
+                if (x2 == x1) {
+                    k = inf;
+                    b = x1;
+                } else {
+                    k = (1.0 * (y2 - y1)) / (x2 - x1);
+                    b = (1.0 * (y1 * dx - x1 * dy)) / dx;
+                int y2 = points[j][1];
+                int dx = x1 - x2;
+                int x2 = points[j][0];
