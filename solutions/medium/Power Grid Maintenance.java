d258b6194d71406df6916e7c55e2c51f14cfcaf8
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-05 13:12:23 +0530

    Add solution for Power Grid Maintenance

---
+++ solutions/medium/Power Grid Maintenance.java
@@ -0,0 +1,33 @@
+// Title: Power Grid Maintenance
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/power-grid-maintenance/
+
+        else if (map[val].isEmpty()) return -2;
+        else{
+            return map[val].first();
+        if (map[val].contains(val)) return val;
+
+    int get(int val){
+    }   
+    void remove(int val){
+        map[val].remove(val);
+        }
+    } 
+
+            map[i].add(i);
+            map[i] = map[val];
+            if (map[val]==null) map[val] =new TreeSet<>();
+            int val = find(i);
+        for (int i=0 ; i<parent.length ; i++){
+    void build(){
+        return true;
+    }
+
+            size[v]+=size[u];
+        }
+            parent[u] = v;
+        else{
+        }
+            size[u]+=size[v];
+            parent[v] = u;
