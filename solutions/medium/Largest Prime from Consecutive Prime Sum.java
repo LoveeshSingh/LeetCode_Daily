e0b72d7cbe522d9a1224c92295279ba97423bef3
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-07 08:32:24 +0530

    Add solution for Largest Prime from Consecutive Prime Sum

---
+++ solutions/medium/Largest Prime from Consecutive Prime Sum.java
@@ -0,0 +1,5 @@
+// Title: Largest Prime from Consecutive Prime Sum
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/largest-prime-from-consecutive-prime-sum/
+
