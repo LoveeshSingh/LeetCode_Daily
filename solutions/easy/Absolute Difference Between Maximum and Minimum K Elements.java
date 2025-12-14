ff9a1f7d98e9b8f5395b93cc26c920968d59011c
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-14 08:04:11 +0530

    Add solution for Absolute Difference Between Maximum and Minimum K Elements

---
+++ solutions/easy/Absolute Difference Between Maximum and Minimum K Elements.java
@@ -0,0 +1,5 @@
+// Title: Absolute Difference Between Maximum and Minimum K Elements
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/absolute-difference-between-maximum-and-minimum-k-elements/
+
