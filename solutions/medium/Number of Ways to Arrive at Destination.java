e811e53d8f7f6ba9ab3e91a972f1362f40ee6c95
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-14 15:45:43 +0530

    Add solution for Number of Ways to Arrive at Destination

---
+++ solutions/medium/Number of Ways to Arrive at Destination.java
@@ -0,0 +1,6 @@
+// Title: Number of Ways to Arrive at Destination
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/
+
+c
