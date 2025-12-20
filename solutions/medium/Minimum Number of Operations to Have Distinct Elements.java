6a17c6701a69fa7c3a991c7f6d021a383757d4c9
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-20 20:07:36 +0530

    Add solution for Minimum Number of Operations to Have Distinct Elements

---
+++ solutions/medium/Minimum Number of Operations to Have Distinct Elements.java
@@ -0,0 +1,5 @@
+// Title: Minimum Number of Operations to Have Distinct Elements
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-number-of-operations-to-have-distinct-elements/
+
