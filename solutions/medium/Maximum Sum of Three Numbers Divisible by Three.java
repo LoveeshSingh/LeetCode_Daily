e07ccc11bd5863c0b1d4610bae1df899a837e944
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-20 20:26:53 +0530

    Add solution for Maximum Sum of Three Numbers Divisible by Three

---
+++ solutions/medium/Maximum Sum of Three Numbers Divisible by Three.java
@@ -0,0 +1,5 @@
+// Title: Maximum Sum of Three Numbers Divisible by Three
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-sum-of-three-numbers-divisible-by-three/
+
