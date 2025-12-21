a55fe75b5e55f964033aa2f3b6832b7f0cc80a14
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-21 23:08:42 +0530

    Add solution for Delete Columns to Make Sorted II

---
+++ solutions/medium/Delete Columns to Make Sorted II.java
@@ -0,0 +1,10 @@
+// Title: Delete Columns to Make Sorted II
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/delete-columns-to-make-sorted-ii/
+
+                return false;
+
+        return true;
+    }
+}
