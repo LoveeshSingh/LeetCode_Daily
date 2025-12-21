ca83362259fd48b145c83a51eca42356fbb5dd2f
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2025-12-21 08:03:57 +0530

    Add solution for Mirror Distance of an Integer

---
+++ solutions/easy/Mirror Distance of an Integer.java
@@ -0,0 +1,5 @@
+// Title: Mirror Distance of an Integer
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/mirror-distance-of-an-integer/
+
