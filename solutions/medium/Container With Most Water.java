a44a239cc3e7d0d34ecd802a796bdbdd5ae06e77
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2026-03-31 19:07:32 +0530

    Add solution for Container With Most Water

--- solutions/medium/Container With Most Water.java
+++ solutions/medium/Container With Most Water.java
@@ -3,4 +3,20 @@
             // Language: Java
             // Link: https://leetcode.com/problems/container-with-most-water/
 
-c
+class Solution {
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length-1;
+        int ans = 0;
+        while(start<end){
+            ans=Math.max(Math.min(height[start],height[end])*(end-start),ans);
+            if (height[start]<height[end]){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+        return ans;
+    }
+}
