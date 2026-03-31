f86fc911fed6176b3f496604036bb0eeb099ea86
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2026-03-31 17:09:39 +0530

    Add solution for Longest Consecutive Sequence

--- solutions/medium/Longest Consecutive Sequence.java
+++ solutions/medium/Longest Consecutive Sequence.java
@@ -3,26 +3,24 @@
             // Language: Java
             // Link: https://leetcode.com/problems/longest-consecutive-sequence/
 
-            parent[parent2]=parent1;
-            size[parent1]+=size[parent2];
-        }  
-    }
-}
-
 class Solution {
     public int longestConsecutive(int[] nums) {
-        DSU dsu = new DSU(nums.length);
         HashMap<Integer,Integer> map = new HashMap<>();
-        int ans=0;
-        for (int i=0 ; i<nums.length ; i++){
-            if (map.containsKey(nums[i])) continue;
-            map.put(nums[i],val++);
-            if (map.containsKey(nums[i]-1)) dsu.union(map.get(nums[i]),map.get(nums[i]-1));
-            if (map.containsKey(nums[i]+1)) dsu.union(map.get(nums[i]),map.get(nums[i]+1));
-            ans=Math.max(ans,dsu.size[dsu.find(map.get(nums[i]))]);
+        for (int val : nums) map.put(val,1);
+        for (int value : nums){
+            while(map.containsKey(val)){
         }
+
+            int cnt=0;
+                cnt+=map.get(val);
+            }
     }
-        else{
-        return ans;
-        int val=0;
+        int max = 0;
+                val++;
+            map.put(value,cnt);
+                map.remove(val);
+            max = Math.max(max,cnt);
+        return max;
+            int val = value;
+                if (map.get(val)<0) break;
 }
