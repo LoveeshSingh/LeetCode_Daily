81f7e03648661d2bcf0e119b107e3e03c5417bdb
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2026-04-07 13:44:37 +0530

    Add solution for Walking Robot Simulation II

---
+++ solutions/medium/Walking Robot Simulation II.java
@@ -0,0 +1,32 @@
+// Title: Walking Robot Simulation II
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/walking-robot-simulation-ii/
+
+            }
+            else{
+                posn[1]=y;
+                num--;
+            int y = posn[1]+step[curr][1];
+            if (x>=0 && y>=0 && x<max[0] && y<max[1]){
+                posn[0]=x;
+            int x = posn[0]+step[curr][0];
+    
+    public void step(int num) {
+        num%=tot;
+        while(num!=0){
+        tot=width*2+height*2-4;
+    }   
+        curr=0;
+        max = new int[]{width,height};
+        dirn = new String[]{"East","North","West","South"};
+        step = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        posn = new int[]{0,0};
+    public Robot(int width, int height) {
+    int[] max;
+    int curr;
+    int tot;
+    int[] posn;
+                curr=(curr+1)%4;
+            }
+        if (num==0) num=tot;
