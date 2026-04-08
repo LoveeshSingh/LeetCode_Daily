7480f35733752409ce326922f443cf9dfd051fdd
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2026-04-08 22:21:23 +0530

    Add solution for XOR After Range Multiplication Queries I

---
+++ solutions/medium/XOR After Range Multiplication Queries I.java
@@ -0,0 +1,6 @@
+// Title: XOR After Range Multiplication Queries I
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/xor-after-range-multiplication-queries-i/
+
+c
