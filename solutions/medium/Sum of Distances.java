751e655756f5594b90f31db8139ed9966b1b70de
LoveeshSingh <72643917+LoveeshSingh@users.noreply.github.com>
2026-04-23 20:40:08 +0530

    Add solution for Sum of Distances

--- solutions/medium/Sum of Distances.java
+++ solutions/medium/Sum of Distances.java
@@ -3,30 +3,24 @@
             // Language: Java
             // Link: https://leetcode.com/problems/sum-of-distances/
 
-        }
-                firstOccurence.put(nums[i],i);
-            }
-                sum.put(nums[i],0L);
             else{
                 sum.put(nums[i],sum.get(nums[i])+i-firstOccurence.get(nums[i]));
+                freq.put(nums[i],freq.get(nums[i])+1);
             }
-        long[] ans = new long[nums.length];
+        }
         HashMap<Integer,Integer> lastOccurence = firstOccurence;
+        long[] ans = new long[nums.length];
+        HashMap<Integer,Integer> done = new HashMap<>(); 
         for (int i=0 ; i<nums.length ; i++){
             long dist = i-lastOccurence.get(nums[i]);
-                freq.put(nums[i],1);
-                freq.put(nums[i],freq.get(nums[i])+1);
-        HashMap<Integer,Integer> done = new HashMap<>(); 
-            lastOccurence.put(nums[i],i);
-            long addn = dist*(left-right);
-        }
-            sum.put(nums[i],sum.get(nums[i])+addn);
-            done.put(nums[i],done.getOrDefault(nums[i],0)+1);
-            ans[i] = sum.get(nums[i]);
-        for (int i=0 ; i<nums.length ; i++){
-            if (!firstOccurence.containsKey(nums[i])){
-        return ans;
-    }
             int left = done.getOrDefault(nums[i],0)-1;
             int right = freq.get(nums[i])-done.getOrDefault(nums[i],0)-1;
+            long addn = dist*(left-right);
+            sum.put(nums[i],sum.get(nums[i])+addn);
+            lastOccurence.put(nums[i],i);
+            done.put(nums[i],done.getOrDefault(nums[i],0)+1);
+            ans[i] = sum.get(nums[i]);
+        }
+        return ans;
+    }
 }
